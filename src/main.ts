039fe3d187d8ae1301b9110e7d4983a7d83e2d9c
s809 <43530948+s809@users.noreply.github.com>
2024-12-28 22:57:03 +0500

    Force JSON on POST requests & all responses

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,37 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Force JSON on POST requests
+app.post("*", express.json({ type: () => true }), (req, res, next) => {
+    if (!Object.keys(req.body).length) {
+        res.status(400).send({
+            message: "JSON body is invalid or missing"
+        });
+        return;
+    }
+    next();
+});
+
 registerRoutes(app);
+
+// Force JSON on responses
+// @ts-ignore
+app.use(function (err, req, res, next) {
+    if (!err.statusCode) {
+        err.statusCode = 500;
+    }
+
+    if (err.statusCode === 500) {
+        console.error(err.message);
+        res.status(err.statusCode).send({
+            message: "Internal server error"
+        });
+    } else {
+        res.status(err.statusCode).send({
+            message: err.message
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`);
